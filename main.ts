ca3b952eeb23b27ec678b1344be7d31e0e8c2106
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2023-01-13 19:07:12 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,8 +9,8 @@ basic.showLeds(`
     `)
 radio.setGroup(90)
 basic.forever(function () {
-    X = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -100, 100)
-    Y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 1023, 0, -100, 100)
+    X = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -50, 50)
+    Y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 0, 1023, -50, 50)
     radio.sendValue("X", X)
     radio.sendValue("Y", Y)
 })
