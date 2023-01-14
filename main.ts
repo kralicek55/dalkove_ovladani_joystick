1500fda0570047ecca369853211d882f7e1730fd
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2023-01-14 17:38:17 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,28 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendValue("MuzesJet", 0)
     basic.showIcon(IconNames.No)
+    basic.pause(500)
+    zobraz_logo()
 })
+function zobraz_logo () {
+    basic.showLeds(`
+        . . # # .
+        . . # # #
+        # # # # .
+        # # # # .
+        . # . # .
+        `)
+}
 input.onButtonPressed(Button.B, function () {
     radio.sendValue("MuzesJet", 1)
     basic.showIcon(IconNames.Yes)
+    basic.pause(500)
+    zobraz_logo()
 })
 let Y = 0
 let X = 0
 radio.setGroup(90)
-basic.showLeds(`
-    . . # # .
-    . . # # #
-    # # # # .
-    # # # # .
-    . # . # .
-    `)
+zobraz_logo()
 basic.forever(function () {
     X = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -20, 20)
     Y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 0, 1023, -50, 50)
